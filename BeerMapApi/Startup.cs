a468c3ba3460eeb017ecc29fd691fbf17ddf1a79
Kevin Wall <kevin.wall@luxinteractive.com>
2019-07-17 14:21:47 -0400

    fix cors

--- BeerMapApi/Startup.cs
+++ BeerMapApi/Startup.cs
@@ -38,6 +38,12 @@ namespace BeerMapApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
